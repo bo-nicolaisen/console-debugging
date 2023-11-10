941394367586f231c3cbad976538a58602c58648
Bo Nicolajsen <>
2023-11-10 10:51:02 +0100

    done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,26 @@
-// write cool JS hwere!!
+// write cool JS here!!
+const myInfo = 'en variabel';
+const myBooks = ["The Catcher in the Rye",
+    "To Kill a Mockingbird",
+    "1984",
+    "Pride and Prejudice",
+    "The Great Gatsby",
+    "The Hobbit",
+    "Harry Potter and the Sorcerer's Stone",
+    "The Lord of the Rings",
+    "The Da Vinci Code",
+    "The Hunger Games"]
+
+console.clear();
+console.time();
+
+console.group('konsol opgave');
+console.log('Jeg er en almindelig konsol log');
+console.info('Jeg er en konsol info: ' + myInfo);
+console.warn('jeg er en advarsel!');
+console.error('jeg er en fejl!!!');
+
+console.table(myBooks);
+console.groupEnd();
+
+console.timeEnd();
